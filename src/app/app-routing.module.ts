0b28c6410acc466d279f33b3f1b696bcc0c2f5dd
trannhatnguyen2 <nguyentn20406c@gmail.com>
2023-03-13 01:41:48 +0700

    modified pageNotFound

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -46,7 +46,6 @@ const routes: Routes = [
   { path: 'bai 16', component: Bai16JsonArrayProductHttpServiceHEComponent },
   { path: 'bai 17', component: Bai17JsonObjectCustomerServiceComponent },
   { path: 'bai 18', component: Bai18JsonArrayGroupCustomersComponent },
-  { path: '**', component: Bai19RoutingPageNotFoundComponent },
   { path: 'bai 13/:id', component: MyComponentComponent },
   { path: 'bai 43/books', component: Bai43HttpGetListOfBooksComponent },
   { path: 'books', component: BooksComponent },
@@ -58,6 +57,7 @@ const routes: Routes = [
   { path: 'bai 54/book-detail/:id', component: Bai54BookDetailComponent },
   { path: 'bai 54/book-create', component: Bai54BookCreateComponent },
   { path: 'bai 54/book-update/:id', component: Bai54BookUpdateComponent },
+  { path: '**', component: Bai19RoutingPageNotFoundComponent },
 ];
 
 @NgModule({
